d17e54662bbb059c9fc4932fd2ccfd60c46f6967
gundogduali <59669294+gundogduali@users.noreply.github.com>
2023-10-27 21:57:57 +0300

    ios 17 changes

--- lib/presentation/route/app_router.dart
+++ lib/presentation/route/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:dog_app/presentation/pages/splash_screen/splash_screen.dart';
 
 part 'app_router.gr.dart';
 
@@ -6,10 +7,10 @@ part 'app_router.gr.dart';
 class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => [
-    AutoRoute(
-      page: SplashRoute.page,
-      initial: true,
-      path: '/',
-    ),
-  ];
+        AutoRoute(
+          page: SplashRoute.page,
+          initial: true,
+          path: '/',
+        ),
+      ];
 }
